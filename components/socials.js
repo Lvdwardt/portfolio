9c416142e4f2a1d97a525557eedd289ef3c4b410
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-06-28 22:09:57 +0200

    readded noreferrer

--- components/socials.js
+++ components/socials.js
@@ -7,6 +7,7 @@ export default function Whatsapp() {
       className="  relative flex items-center justify-center overflow-hidden rounded-3xl bg-greenlight dark:bg-[#8D5BE9] sm:order-9 sm:dark:bg-[#CE81C7]"
       href="https://api.whatsapp.com/send?phone=31630702130"
       target={"_blank"}
+      rel="noreferrer"
     >
       <FaWhatsapp className="text-[100px] text-white" />
     </a>
@@ -18,6 +19,7 @@ export function Github() {
       className=" relative flex items-center justify-center overflow-hidden rounded-3xl bg-bluelight dark:bg-[#CE81C7] sm:order-8 xl:order-5"
       href="https://github.com/Lvdwardt"
       target={"_blank"}
+      rel="noreferrer"
     >
       <FaGithub className="text-[100px] text-white" />
     </a>
@@ -29,6 +31,7 @@ export function Discord() {
       className=" relative flex items-center justify-center overflow-hidden rounded-3xl bg-greenlight dark:bg-[#CE81C7] sm:order-5 xl:order-8"
       href="https://discord.com/users/396931979020337153"
       target={"_blank"}
+      rel="noreferrer"
     >
       <FaDiscord className="text-[100px] text-white" />
     </a>
@@ -40,6 +43,7 @@ export function Mail() {
       className="relative flex items-center justify-center overflow-hidden rounded-3xl bg-greenlight dark:bg-[#CE81C7] sm:order-12 sm:bg-bluelight sm:dark:bg-[#8D5BE9]"
       href="mailto:contact@leonvandewardt.nl"
       target={"_blank"}
+      rel="noreferrer"
     >
       <FaEnvelope className="text-[100px] text-white" />
     </a>
