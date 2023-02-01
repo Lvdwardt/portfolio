11b02438e1fecf281bf749357598612ca76e25eb
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-02-01 02:05:42 +0100

    about page fix for mobile

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -40,58 +40,63 @@ export default function About() {
         <title>About</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col gap-6 px-24 pt-4 pb-8">
-        <div className="flex w-full flex-1 flex-col rounded-[2rem] bg-card p-4 px-10">
-          <div className="pb-4">
-            <Image
-              src="/images/memoji/mac.webp"
-              alt="Leon"
-              width={80}
-              height={120}
-            />
+      <div className="flex w-full justify-center">
+        <div className="mx-6 flex max-w-[1200px] flex-col gap-6 pt-4 pb-8">
+          <div className="flex w-full flex-1 flex-col rounded-[2rem] bg-card p-4 px-10">
+            <div className="pb-4">
+              <Image
+                src="/images/memoji/mac.webp"
+                alt="Leon"
+                width={80}
+                height={120}
+              />
+            </div>
+            <h2>hi, I&apos;m Leon</h2>
+            <h2>ABOUT ME</h2>
           </div>
-          <h2>hi, I&apos;m Leon</h2>
-          <h2>ABOUT ME</h2>
-        </div>
-        <div className="flex w-full flex-1 flex-col gap-4 rounded-[2rem] bg-card py-4 px-10">
-          <h2 className="text-2xl font-bold text-text">Experiences</h2>
-          {experiences.map((experience) => (
-            <div className="flex items-start gap-4 pb-4" key={experience.name}>
-              <a href={experience.link}>
-                <Image
-                  src={experience.image}
-                  alt={experience.name}
-                  width={60}
-                  height={60}
-                  className="rounded-xl bg-black"
-                />
-              </a>
-              <div className="flex w-full flex-col">
-                <h3 className="mt-[-4px] align-text-top text-lg font-bold text-text">
-                  {experience.role}
-                </h3>
-                <div className="flex justify-between text-sm text-gray-400">
-                  <div className="flex flex-col break-words sm:flex-row">
-                    {!experience.small && <h4>{experience.name} - </h4>}
-                    {experience.small && (
-                      <>
-                        <h4 className="sm:hidden">{experience.small}</h4>
-                        <h4 className="hidden sm:block">
-                          {experience.name} -{" "}
-                        </h4>
-                      </>
-                    )}
-                    <h4 className="w-20 sm:ml-1">{experience.hours}</h4>
+          <div className="flex w-full flex-1 flex-col gap-4 rounded-[2rem] bg-card py-4 px-10">
+            <h2 className="text-2xl font-bold text-text">Experiences</h2>
+            {experiences.map((experience) => (
+              <div
+                className="flex items-start gap-4 pb-4"
+                key={experience.name}
+              >
+                <a href={experience.link}>
+                  <Image
+                    src={experience.image}
+                    alt={experience.name}
+                    width={60}
+                    height={60}
+                    className="rounded-xl bg-black"
+                  />
+                </a>
+                <div className="flex w-full flex-col">
+                  <h3 className="mt-[-4px] align-text-top text-lg font-bold text-text">
+                    {experience.role}
+                  </h3>
+                  <div className="flex justify-between text-sm text-gray-400">
+                    <div className="flex flex-col break-words sm:flex-row">
+                      {!experience.small && <h4>{experience.name} - </h4>}
+                      {experience.small && (
+                        <>
+                          <h4 className="sm:hidden">{experience.small}</h4>
+                          <h4 className="hidden sm:block">
+                            {experience.name} -{" "}
+                          </h4>
+                        </>
+                      )}
+                      <h4 className="w-20 sm:ml-1">{experience.hours}</h4>
+                    </div>
+                    <h4 className="text-end">{experience.dates}</h4>
                   </div>
-                  <h4 className="text-end">{experience.dates}</h4>
-                </div>
-                <div className="font-thin">
-                  <p className="line-clamp-2">{experience.description}</p>
-                  {/* <p className="text-right">read more</p> */}
+                  <div className="font-thin">
+                    <p className="line-clamp-2">{experience.description}</p>
+                    {/* <p className="text-right">read more</p> */}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </>
