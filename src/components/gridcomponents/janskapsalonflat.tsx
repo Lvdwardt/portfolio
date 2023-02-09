13eac2c074e0cc83633489d1ba561e9a358bd93d
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-02-09 16:55:28 +0100

    changed the link reveal effect on project cards

--- src/components/gridcomponents/janskapsalonflat.tsx
+++ src/components/gridcomponents/janskapsalonflat.tsx
@@ -5,7 +5,7 @@ import { ImArrowUpRight2 } from "react-icons/im";
 export default function Janskapsalonflat() {
   return (
     <div className="group relative h-full w-full">
-      <div className=" absolute h-[500px] w-[500px] translate-x-12 translate-y-8 overflow-hidden rounded-full bg-pg sm:translate-y-[-12.5rem] sm:rounded-t-none" />
+      <div className="absolute h-[500px] w-[500px] translate-x-12 translate-y-8 overflow-hidden rounded-full bg-pg sm:translate-y-[-12.5rem] sm:rounded-t-none" />
       <div className="block translate-y-[-170px] select-none sm:translate-y-0">
         <Image
           src="/images/jkflat2.webp"
@@ -15,20 +15,22 @@ export default function Janskapsalonflat() {
           loading="lazy"
         />
       </div>
-      <div className="absolute bottom-4 left-4 flex h-8 w-8 items-center justify-end gap-2 rounded-full bg-dw p-2 text-text transition-all duration-300 ease-in group-hover:w-40 ">
-        <Link
-          href="/projects/janskapsalon"
-          passHref
-          title="go to the jans kapsalon project page"
-        >
-          <ImArrowUpRight2 className="" />
-        </Link>
-        <Link
-          href="/projects/janskapsalon"
-          className=" absolute left-2 opacity-0 transition-all duration-200 group-hover:animate-fadeInRight group-hover:opacity-100 group-hover:delay-[250ms] hover:opacity-100 hover:delay-[250ms]"
-        >
-          Janskapsalon
-        </Link>
+      <div className="absolute bottom-2 left-4 m-2 box-border flex h-12 w-8 items-center justify-center transition-all duration-500 group-hover:w-[162px] ">
+        <div className="rounded-full border-transparent transition-all duration-300 hover:border-4 hover:border-white/20">
+          <div className="flex h-8 w-8 items-center justify-end gap-4 overflow-hidden rounded-full bg-dw p-2 text-text transition-all duration-500 group-hover:w-[162px] hover:w-[162px] ">
+            <Link
+              href="/projects/janskapsalon"
+              passHref
+              title="go to the jans kapsalon project page"
+              className="flex items-center gap-2"
+            >
+              <h1 className="translate-x-[-10px] opacity-0 transition-all delay-200 duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+                Janskapsalon
+              </h1>
+              <ImArrowUpRight2 />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/gridcomponents/janskapsalonsmall.tsx
+++ src/components/gridcomponents/janskapsalonsmall.tsx
@@ -4,7 +4,7 @@ import { ImArrowUpRight2 } from "react-icons/im";
 export default function Janskapsalonsmall() {
   return (
     <div className="group relative h-full w-full overflow-hidden rounded-[2rem]">
-      <div className=" absolute h-[500px] w-[500px] translate-x-12 translate-y-8 overflow-hidden rounded-full bg-pg sm:translate-y-[-12.5rem] sm:rounded-t-none" />
+      <div className="absolute h-[500px] w-[500px] translate-x-12 translate-y-8 overflow-hidden rounded-full bg-pg sm:translate-y-[-12.5rem] sm:rounded-t-none" />
       <div className="block w-[250] translate-y-[-80px] translate-x-[98px] select-none ">
         <Image
           src="/images/jk.webp"
@@ -13,16 +13,22 @@ export default function Janskapsalonsmall() {
           height={3292}
         />
       </div>
-      <div className=" absolute bottom-4 left-4 flex h-8 w-8 items-center justify-end gap-2 rounded-full bg-dw p-2 text-text transition-all duration-300 ease-in group-hover:w-40 ">
-        <Link href="/projects/janskapsalon" passHref>
-          <ImArrowUpRight2 className="" />
-        </Link>
-        <Link
-          href="/projects/janskapsalon"
-          className=" absolute left-2 opacity-0 transition-all duration-200 group-hover:animate-fadeInRight group-hover:opacity-100 group-hover:delay-[250ms] hover:opacity-100 hover:delay-[250ms]"
-        >
-          Janskapsalon
-        </Link>
+      <div className="absolute bottom-2 left-4 m-2 box-border flex h-12 w-8 items-center justify-center transition-all duration-700 group-hover:w-[162px] ">
+        <div className="rounded-full border-transparent transition-all duration-300 hover:border-4 hover:border-white/20">
+          <div className="flex h-8 w-8 items-center justify-end gap-4 overflow-hidden rounded-full bg-dw p-2 text-text transition-all duration-700 group-hover:w-[162px] hover:w-[162px] ">
+            <Link
+              href="/projects/janskapsalon"
+              passHref
+              title="go to the jans kapsalon project page"
+              className="flex items-center gap-2"
+            >
+              <h1 className="translate-x-[-20px] opacity-0 transition-all delay-500 duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+                Janskapsalon
+              </h1>
+              <ImArrowUpRight2 />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
