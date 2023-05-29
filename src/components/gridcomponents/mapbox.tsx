d0c159357352363249e07befe3116424d9e58854
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-05-29 23:57:55 +0200

    feat(mapbox.tsx): add support for dark and light themes feat(mapbox.tsx): center map to marker coordinates feat(mapbox.tsx): add marker to map feat(mapbox.tsx): add contribution info button feat(mapbox.tsx): add mapbox attribution fix(mapbox.tsx): fix zooming transition issue fix(mapbox.tsx): fix marker scale issue

--- src/components/gridcomponents/mapbox.tsx
+++ src/components/gridcomponents/mapbox.tsx
@@ -1,168 +1,251 @@
 "use client";
-import React, {
-  useRef,
-  useEffect,
-  useState,
-  useCallback,
-  startTransition,
-} from "react";
-import { Map } from "react-map-gl";
-import { FaMinus, FaPlus } from "react-icons/fa";
-import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState, startTransition } from "react";
+import {
+  FaInfoCircle,
+  FaMinus,
+  FaPlus,
+  FaRegCommentDots,
+  FaRegCopyright,
+} from "react-icons/fa";
+import { Map, MapRef, Marker } from "react-map-gl";
 import Image from "next/image";
+import { useTheme } from "next-themes";
+import { SiMapbox } from "react-icons/si";
+import Link from "next/link";
+import { AiFillCloseCircle } from "react-icons/ai";
 import clsx from "clsx";
-import MapsData from "./maps/mapsData";
 
-const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
-//eslint-disable-next-line
-export default async function Mapbox() {
-  const data = await MapsData();
+export default async function Mapbox({ data }: { data: any }) {
+  let coords = {
+    longitude: 51.92735,
+    latitude: 5.5735,
+  };
 
-  const router = useRouter();
+  //if data[0].longitude is 0 set to default
+  if (data && data[0].longitude !== 0) {
+    coords = {
+      longitude: data[0].longitude,
+      latitude: data[0].latitude,
+    };
+  }
 
-  const mapRef = useRef(null);
-  const [lng, setLng] = useState(5.5735);
-  const [lat, setLat] = useState(51.92735);
-  const [zoom, setZoom] = useState(11);
-  const [style, setStyle] = useState(
-    "mapbox://styles/leonvdw/ckza19352000615rsk08y22f3"
-  );
-
-  const onMapLoad = useCallback(() => {
-    console.log(lat, lng);
-    if (!mapRef.current) return;
-    startTransition(() => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      mapRef.current.getCenter() &&
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //@ts-ignore
-        setLng(mapRef.current.getCenter().lng.toFixed(4));
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      setLat(mapRef.current.getCenter().lat.toFixed(4));
-    });
-  }, []);
+  const [awake, setAwake] = useState(false);
+  const [contributionOpen, setContributionOpen] = useState(false);
+  const { resolvedTheme } = useTheme();
+  const [transitioning, setTransitioning] = useState(false);
 
   useEffect(() => {
-    const htmlElement = document.querySelector<HTMLElement>("html");
-    let timeout: NodeJS.Timeout;
-
-    const observer = new MutationObserver((mutations) => {
-      clearTimeout(timeout);
-
-      timeout = setTimeout(() => {
+    if (transitioning) {
+      setTimeout(() => {
         startTransition(() => {
-          mutations.forEach((mutation) => {
-            if (mutation.attributeName === "data-theme") {
-              htmlElement?.getAttribute("data-theme") === "light"
-                ? setStyle("mapbox://styles/mapbox/light-v10")
-                : setStyle("mapbox://styles/leonvdw/ckza19352000615rsk08y22f3");
-              router.refresh();
-            }
-          });
+          setTransitioning(false);
         });
-      }, 100);
-    });
-
-    if (!htmlElement) {
-      return;
+      }, 1000);
     }
+  }, [transitioning]);
 
-    observer.observe(htmlElement, {
-      attributes: true,
-      attributeFilter: ["data-theme"],
-    });
+  const [zoom, setZoom] = useState(11);
+  const [exactZoom, setExactZoom] = useState(11);
+  const [style, setStyle] = useState("mapbox://styles/mapbox/dark-v11");
 
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
+  const mapRef = useRef<MapRef>(null);
 
   const plus = () => {
     startTransition(() => {
+      setTransitioning(true);
       setZoom(zoom + 2);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      mapRef.current?.flyTo({ zoom: zoom + 2, duration: 1000 });
     });
   };
 
   const minus = () => {
     startTransition(() => {
+      setTransitioning(true);
       setZoom(zoom - 2);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      mapRef.current?.flyTo({ zoom: zoom - 2, duration: 1000 });
     });
   };
-  const [awake, setAwake] = useState(false);
+
+  // if resolvedTheme changes, change map style
+  useEffect(() => {
+    if (resolvedTheme === "dark") {
+      startTransition(() => {
+        setStyle("mapbox://styles/mapbox/dark-v11");
+      });
+    } else {
+      startTransition(() => {
+        setStyle("mapbox://styles/mapbox/light-v10");
+      });
+    }
+  }, [resolvedTheme]);
+
+  //center to marker
+  useEffect(() => {
+    mapRef.current?.flyTo({
+      center: [coords.longitude, coords.latitude],
+      zoom: zoom,
+      duration: 1000,
+    });
+    startTransition(() => {
+      setExactZoom(zoom);
+    });
+  }, [zoom]);
+
+  const setUncontrolledZoom = () => {
+    if (transitioning) {
+      return;
+    }
+
+    startTransition(() => {
+      setExactZoom(mapRef.current?.getZoom() || 10);
+    });
+  };
+
+  const marker = () => {
+    return (
+      <div
+        className="h-24 w-24 rounded-full border-4 border-primary bg-trans opacity-80 transition-all duration-1000"
+        style={{
+          //scale based on the zoom, the larger the zoom the bigger the marker //min scale is 0.1 //max is 1 //min zoom is -1 //max is 11
+          // from zoom 2 scale is 0.1, then normal calculation.
+
+          transform: `scale(${Math.min(Math.max(0.1, (exactZoom - 2) / 10), 1)}
+          )`,
+        }}
+      >
+        <Image
+          src="/images/memoji/sleep.webp"
+          alt="memoji sleeping"
+          width={45}
+          height={45}
+          onClick={() => setAwake(true)}
+          className={clsx(
+            "pointer-events-auto absolute inset-0 m-auto transform rounded-full transition duration-500 ease-in-out hover:animate-scale",
+            awake ? "opacity-0" : "opacity-100"
+          )}
+        />
+        <Image
+          src="/images/memoji/knocked.webp"
+          alt="memoji knocked out"
+          width={62}
+          height={62}
+          className={clsx(
+            "pointer-events-none absolute inset-0 top-2 m-auto transform rounded-full transition duration-500 ease-in-out ",
+            awake ? "opacity-100" : "opacity-0"
+          )}
+        />
+      </div>
+    );
+  };
 
   return (
-    <div>
+    <div className="relative h-full w-full">
       <Map
-        reuseMaps
+        onZoom={() => setUncontrolledZoom()}
         ref={mapRef}
-        attributionControl={false}
-        initialViewState={{
-          longitude: lng,
-          latitude: lat,
-          zoom: zoom,
-        }}
-        style={{ width: 280, height: 280 }}
+        maxZoom={11}
+        minZoom={0.0000001}
         mapStyle={style}
-        interactive={false}
-        onMove={onMapLoad}
-        mapboxAccessToken={MAPBOX_TOKEN}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        initialViewState={{
+          longitude: coords.longitude,
+          latitude: coords.latitude,
+          zoom: 11,
+        }}
+        projection={"globe"}
+        attributionControl={false}
       >
-        <div className=" absolute inset-0 bottom-8 m-auto h-24 w-24 rounded-full border-4 border-primary bg-trans opacity-80">
-          <Image
-            src="/images/memoji/sleep.webp"
-            alt="memoji sleeping"
-            width={45}
-            height={45}
-            onClick={() => setAwake(true)}
-            className={clsx(
-              "absolute inset-0 m-auto transform rounded-full transition duration-500 ease-in-out hover:animate-scale",
-              awake ? "opacity-0" : "opacity-100"
-            )}
-          />
-          <Image
-            src="/images/memoji/knocked.webp"
-            alt="memoji knocked out"
-            width={62}
-            height={62}
-            className={clsx(
-              "pointer-events-none absolute inset-0 top-2 m-auto transform rounded-full transition duration-500 ease-in-out ",
-              awake ? "opacity-100" : "opacity-0"
-            )}
-          />
-        </div>
-
-        <button
-          className={
-            zoom === 1
-              ? "hidden"
-              : "absolute bottom-12 left-8 flex h-8 w-8 items-center justify-center rounded-full bg-secondary  text-text"
-          }
-          title="Zoom out"
-          onClick={minus}
+        <Marker
+          draggable={false}
+          longitude={coords.longitude}
+          latitude={coords.latitude}
         >
-          <FaMinus />
-        </button>
-
-        <button
-          className={
-            zoom >= 11
-              ? "hidden"
-              : "absolute bottom-12 right-8 flex h-8 w-8 items-center justify-center rounded-full bg-secondary text-text"
-          }
-          title="Zoom in"
-          onClick={plus}
-        >
-          <FaPlus />
-        </button>
+          {marker()}
+        </Marker>
       </Map>
+      <button
+        className="absolute bottom-4 left-4 z-50 flex items-center"
+        onClick={() =>
+          startTransition(() => {
+            setContributionOpen(!contributionOpen);
+          })
+        }
+      >
+        {/* <FaInfoCircle className="mr-1 text-xs text-text" /> */}
+        {contributionOpen ? (
+          <AiFillCloseCircle className="mr-1 text-xs text-text" />
+        ) : (
+          <FaInfoCircle className="mr-1 text-xs text-text" />
+        )}
+      </button>
+      {contributionOpen && (
+        <div className="absolute bottom-0 left-0 z-40 flex w-full flex-col items-start justify-center rounded-md bg-secondary p-4 pb-10 shadow-md">
+          <p className="text-xs font-semibold text-text">Mapbox GL JS </p>
+          <Link
+            href="https://www.mapbox.com/about/maps/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center"
+          >
+            <FaRegCopyright className="mr-1 text-xs text-text" />
+            <span className="text-xs text-text">mapbox</span>
+          </Link>
+          <Link
+            href="https://www.openstreetmap.org/copyright"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center"
+          >
+            <FaRegCopyright className="mr-1 text-xs text-text" />
+            <span className="text-xs text-text">OpenStreetMap</span>
+          </Link>
+          <Link
+            href="https://www.mapbox.com/map-feedback/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center"
+          >
+            <FaRegCommentDots className="mr-1 text-xs text-text" />
+            <span className="text-xs text-text">Improve this map</span>
+          </Link>
+        </div>
+      )}
+
+      <Link
+        href="https://www.mapbox.com/about/maps/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="absolute bottom-4 right-4 flex items-center"
+      >
+        <SiMapbox className="" />
+        <span className=" mx-1 select-none text-xs font-bold text-text">
+          mapbox
+        </span>
+      </Link>
+
+      <button
+        className={clsx(
+          transitioning && "opacity-40",
+          zoom <= -1 && "hidden",
+          "absolute bottom-12 left-8 flex h-8 w-8 items-center justify-center rounded-full bg-secondary text-text"
+        )}
+        title="Zoom out"
+        onClick={minus}
+        disabled={transitioning}
+      >
+        <FaMinus />
+      </button>
+
+      <button
+        className={clsx(
+          transitioning && "opacity-40",
+          zoom >= 11 && "hidden",
+          "absolute bottom-12 right-8 flex h-8 w-8 items-center justify-center rounded-full bg-secondary text-text"
+        )}
+        title="Zoom in"
+        onClick={plus}
+        disabled={transitioning}
+      >
+        <FaPlus />
+      </button>
     </div>
   );
 }
