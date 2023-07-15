cc6b7c69ed137a212485cdf1916a5a870516ddd7
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-07-15 18:14:40 +0200

    feat(route.ts): add new route for GET requests to handle map routes
    feat(traveldata.ts): add list of city names and countries for travel data
    
    refactor(maps.tsx): remove unused imports and refactor Maps component to improve readability and performance

--- src/app/(main)/travels/traveldata.ts
+++ src/app/(main)/travels/traveldata.ts
@@ -69,6 +69,145 @@ export const capitalNames = [
   "London",
 ];
 
+export const cityNames = [
+  {
+    name: "Amsterdam",
+    country: "nl",
+  },
+  {
+    name: "Antwerp",
+    country: "be",
+  },
+  {
+    name: "Brussels",
+    country: "be",
+  },
+  {
+    name: "Paris",
+    country: "fr",
+  },
+  {
+    name: "Mimizan",
+    country: "fr",
+  },
+  {
+    name: "Barcelona",
+    country: "es",
+  },
+  {
+    name: "Madrid",
+    country: "es",
+  },
+  {
+    name: "Ibiza",
+    country: "es",
+  },
+  {
+    name: "Porto",
+    country: "pt",
+  },
+  {
+    name: "Stuttgart",
+    country: "de",
+  },
+  {
+    name: "Berlin",
+    country: "de",
+  },
+  {
+    name: "Zurich",
+    country: "ch",
+  },
+  {
+    name: "Basel",
+    country: "ch",
+  },
+  {
+    name: "Geneva",
+    country: "ch",
+  },
+  {
+    name: "Zermatt",
+    country: "ch",
+  },
+  {
+    name: "Milan",
+    country: "it",
+  },
+  {
+    name: "Rome",
+    country: "it",
+  },
+  {
+    name: "Naples",
+    country: "it",
+  },
+  {
+    name: "Bari",
+    country: "it",
+  },
+  {
+    name: "Catania",
+    country: "it",
+  },
+  {
+    name: "Cagliari",
+    country: "it",
+  },
+  {
+    name: "Prague",
+    country: "cz",
+  },
+  {
+    name: "Vienna",
+    country: "at",
+  },
+  {
+    name: "Bratislava",
+    country: "sk",
+  },
+  {
+    name: "Budapest",
+    country: "hu",
+  },
+  {
+    name: "Zagreb",
+    country: "hr",
+  },
+  {
+    name: "Pula",
+    country: "hr",
+  },
+  {
+    name: "Zadar",
+    country: "hr",
+  },
+  {
+    name: "Porec",
+    country: "hr",
+  },
+  {
+    name: "Krakow",
+    country: "pl",
+  },
+  {
+    name: "Zator",
+    country: "pl",
+  },
+  {
+    name: "Sofia",
+    country: "bg",
+  },
+  {
+    name: "Bucharest",
+    country: "ro",
+  },
+  {
+    name: "Dubai",
+    country: "ae",
+  },
+];
+
 export const trips = [
   {
     id: "1",
