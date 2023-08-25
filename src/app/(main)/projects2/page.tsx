148dba2ac3ff6c739d5739235ec3b2a5150091ea
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-08-25 22:40:34 +0200

    fix(page.tsx): add ts-expect-error comment to ignore type error for server-component in About component

--- src/app/(main)/projects2/page.tsx
+++ src/app/(main)/projects2/page.tsx
@@ -20,6 +20,7 @@ export default function Projects() {
         <div className="w-full overflow-visible transition-all duration-300 ease-in ">
           <div className="mx-auto grid w-full max-w-[320px] grid-cols-1 gap-5 px-4 pb-6 pt-2 [grid-auto-columns:265px] [grid-auto-rows:265px] sm:max-w-[640px] sm:grid-cols-2 xl:max-w-[1200px] xl:grid-cols-4 xl:grid-rows-[265px,265px] xl:px-0 ">
             <div className="order-1 col-span-1 rounded-[2rem] bg-card p-4 pl-6 pt-[18px] sm:col-span-2 xl:col-span-1">
+              {/* @ts-expect-error server-component */}
               <About />
             </div>
             <div className="group relative order-3 overflow-hidden rounded-[2rem] bg-card sm:order-3 sm:row-span-2">
