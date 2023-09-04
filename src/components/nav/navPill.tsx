c7dfdd4de5c9d0531e48fc6c78e464dd297e1702
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-09-04 20:55:00 +0200

    fix(navPill.tsx): add / to href to fix navigation breaking on /projects/...

--- src/components/nav/navPill.tsx
+++ src/components/nav/navPill.tsx
@@ -5,18 +5,16 @@ import { usePathname } from "next/navigation";
 
 export default function NavPill() {
   const path = usePathname();
-  //path == /about or /projects/janskapsalon
-  //trim the path to /about or /projects
   let currentRoute = path.split("/")[1];
   if (currentRoute === "") currentRoute = "/";
 
   const routes = [
     { name: "Home", href: "/", place: 1, width: 17.5, left: 2 },
-    { name: "About", href: "about", place: 2, width: 18.5, left: 25.1 },
-    { name: "Projects", href: "projects", place: 3, width: 22, left: 49.5 },
-    { name: "Travels", href: "travels", place: 4, width: 20, left: 78 },
+    { name: "About", href: "/about", place: 2, width: 18.5, left: 25.1 },
+    { name: "Projects", href: "/projects", place: 3, width: 22, left: 49.5 },
+    { name: "Travels", href: "/travels", place: 4, width: 20, left: 78 },
   ];
-  const currentPlace = routes.find((route) => route.href === currentRoute)
+  const currentPlace = routes.find((route) => route.href.includes(currentRoute))
     ?.place as number;
 
   //calculate difference between current place and the place of the clicked route
